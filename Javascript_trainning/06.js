dcb36c1e73fd1f8bda858442f4907387cbb66e3e
WhorideChicken <wlsdndnjs@gmail.com>
2024-10-22 02:08:41 +0900

    Feat : JS에서 조건문을 사용할 수 있는 조건부 실행, falsy한 값들에 대해 알았다.
    - JS에서 조건문을 사용할 수 있는 조건부 실행, falsy한 값들에 대해 알았다.

--- Javascript_trainning/06.js
+++ Javascript_trainning/06.js
@@ -6,6 +6,8 @@ if(x>50)
     console.log("5보다 큼");
 else if(x>0)
     console.log("0보다 큼")
+else if(x<0 && x===-1)
+    console.log("-1 이네?");
 else
     console.log("음수였네?");
 
@@ -27,3 +29,12 @@ switch(num)
         console.log("3이다");
         break;
 }
+
+//조건부 실행
+x < 0 && console.log("0보다 작다");
+
+//삼항 연산자와 단축평가
+let result; //result는 undefined
+let z = result || 20;
+console.log(z);
+//해설 : result가 undefined면 z는 20으로 할당해줘라는 의미
---
+++ Javascript_trainning/07.js
@@ -0,0 +1,4 @@
+//falsy한 값, truthy한 값
+//조건문에는 true, false가 들어가야하는데 이때 가능한 값들이 뭐가 있을지
+//true, false가 아닌 값들이 들어갔을 때 true가 되면 truthy한 값 false가 되면 falsy한 값이라하는ㄷ
+//0, "", null, undefined, NaN은 falsy한 값이다.
