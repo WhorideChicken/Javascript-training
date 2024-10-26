6dd84a349fc80049dcbd8d92104de01417320c37
WhorideChicken <wlsdndnjs@gmail.com>
2024-10-27 03:26:14 +0900

    Feat : 퀴즈 문제 추가
    -html의 id값이나 class값에 접근하여 기능을 추가하는 퀴즈

--- Javascript_trainning/5-09.js
+++ Javascript_trainning/5-09.js
@@ -16,3 +16,5 @@ function outerFunc(){
 
 
 outerFunc();
+
+
--- Javascript_trainning/5-11.js
+++ Javascript_trainning/5-11.js
@@ -9,5 +9,11 @@ function outer(){
     return inner;
 }
 
+//outer()를 실행하서 innerFunc에 담는다. ()가 있으면 실행한것
+//ounter return 부분을 innerFunc에 담은것이다.
 const innerFunc = outer();
+//즉여기서 outer()의 실행컨텍스트는 사라진다.
+//inner()안에는 x가 있고 x는 outer 실행컨텍스트에 있다.
+//outer는 실행하고 사라졌는데 inner에서 x를 참조할 수 있다 -> 클로저
+
 innerFunc();
